b69513d6d294a59aec19ea62098f22be115b41ef
jordanzee <jordanzabala09@gmail.com>
2018-03-27 17:41:52 +0000

    add DB

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://jordanzee:jordan@ds123919.mlab.com:23919/yelpcamps");
+// mongoose.connect(process.env.DATABASEURL);
+mongoose.connect("mongodb://jordanzee:jordan@ds123919.mlab.com:23919/yelpcamps");
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
